b754fea770467d25f0ec060bfa80a04407730206
ntnhan-ds <20120151@student.hcmus.edu.vn>
2024-04-09 17:35:48 +0700

    read file

--- app/models/url.py
+++ app/models/url.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("text_sentiment", views.sentiment_analysis),
+    path("json_file_sentiment", views.sentiment_file_json_analysis),
+    path("csv_file_sentiment", views.sentiment_file_csv_analysis),
 ]
--- app/models/views.py
+++ app/models/views.py
@@ -4,6 +4,12 @@ from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 import json
 import datetime
+import re
+import openpyxl
+from openpyxl import load_workbook
+import io
+import pandas as pd
+from io import StringIO
 
 
 @csrf_exempt
@@ -23,6 +29,57 @@ def sentiment_analysis(request):
         return HttpResponse("Method not allowed", status=405)
 
 
+def find_filename(data):
+    match = re.search(r'filename="([^"]+)"', data)
+    if match:
+        filename = match.group(1)
+        return filename
+    else:
+        return None
+
+
+def extract_json_string(data):
+    match = re.search(r"\[([\s\S]*)\]", data)
+    if match:
+        json_string = match.group(0)  # get [ ]
+        return json_string
+    else:
+        return None
+
+
 @csrf_exempt
-def get_file(request):
-    return "file"
+def sentiment_file_json_analysis(request):
+    if request.method == "POST":
+        data = request.body.decode("utf-8")
+        json_data = json.loads(extract_json_string(data))
+
+        print(json_data)
+
+        return JsonResponse({"data": data})
+
+    else:
+        return HttpResponse("Method not allowed.")
+
+
+def extract_csv_from_string(data_bytes):
+    data_string = data_bytes.decode()
+    # find index of start and end of csv data
+    start_index = data_string.find("\r\n\r\n") + len("\r\n\r\n")
+    end_index = data_string.rfind("\r\n----------------------------")
+
+    csv_data = data_string[start_index:end_index].strip()
+    return csv_data
+
+
+@csrf_exempt
+def sentiment_file_csv_analysis(request):  # error when has many , in csv
+    if request.method == "POST":
+        data = request.body
+        csv_data = extract_csv_from_string(data)
+        # read data to csv
+        df = pd.read_csv(StringIO(csv_data))
+
+        print(df)
+        return JsonResponse({"data": csv_data})
+    else:
+        return HttpResponse("Method not allowed.")
