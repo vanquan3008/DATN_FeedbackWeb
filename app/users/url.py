72ab4a0db27047f84e0c6a9e7f1d9da6c4eb9e1e
ntnhan-ds <20120151@student.hcmus.edu.vn>
2024-04-27 19:57:07 +0700

    add url

--- app/users/url.py
+++ app/users/url.py
@@ -9,14 +9,15 @@ urlpatterns = [
     path("json_analysis", views.analyze_json_file),
     path("csv_analysis", views.analyze_csv_file),
     path("text_analysis", views.analyze_text),
-    
     path("txt_analysis", views.analyze_txt_file),
     path("create_post", views.create_post),
     path("post_comment_to_status", views.post_comment_to_status),
     path("get_all_post_by_userid", views.get_all_post_by_userid),
     path("get_all_post", views.get_all_post),
     path("delete_post", views.delete_post),
-    #Comment
+    # Comment
     path("get_all_comments_on_post", views.get_all_comments_on_post),
-    path("get_sentiment_comments_on_post", views.static_all_comments_on_post)
+    path("get_sentiment_comments_on_post", views.static_all_comments_on_post),
+    # Based-aspect level
+    path("text_detail_analysis", views.analyze_detail_text),
 ]
