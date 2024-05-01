bbf59c9bbf4b8d20fec944f053b6bf21441a7657
VanQuan <vandinhminhquan2002@gmail.com>
2024-05-01 23:48:15 +0700

    fix architecture be

--- app/app/settings.py
+++ app/app/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "users",
     "models",
+    "posts",
+    "comments"
 ]
 
 MIDDLEWARE = [
--- app/users/admin.py
+++ app/users/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from .models import User, Post, Report, Detail_post, Result_file, Result_text
+from .models import User
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Report)
-admin.site.register(Detail_post)
-admin.site.register(Result_file)
-admin.site.register(Result_text)
+
