87731ae32ce1c18f6ad9beaa97afc443989ae3f0
ntnhan-ds <20120151@student.hcmus.edu.vn>
2024-05-23 14:45:58 +0700

    crawl and analysis tiki

--- app/others/urls.py
+++ app/others/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("comments_shopee_analysis", views.comments_shopee_analysis),
     path("comments_lazada_analysis", views.comments_lazada_analysis),
+    path("comments_tiki_analysis", views.comments_tiki_analysis),
 ]
--- app/others/views.py
+++ app/others/views.py
@@ -23,7 +23,7 @@ from models.views import (
 
 #################### SHOPEE ####################
 
-headers = {
+headers_shopee = {
     "Accept": "*/*",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "vi,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
@@ -81,7 +81,7 @@ def crawl_shopee_comments(url):
     shop_id, item_id = extract_idshop_iditem_from_url_shopee(url)
     all_comments = []
     for offset in range(0, 1000, 6):
-        ratings = fetch_shopee_ratings(shop_id, item_id, headers, offset=offset)
+        ratings = fetch_shopee_ratings(shop_id, item_id, headers_shopee, offset=offset)
         if not ratings:
             break
         for rating in ratings:
@@ -201,6 +201,105 @@ def comments_lazada_analysis(request):
                 # "positive_count": pos_count,
                 # "negative_count": neg_count,
                 # "neutral_count": neu_count,
+            },
+            status=200,
+        )
+    else:
+        return JsonResponse(
+            {"error": "Only POST requests are allowed for this endpoint"}, status=500
+        )
+
+
+#################### TIKI ####################
+
+headers_tiki = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "vi,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+    "Referer": "https://tiki.vn/",
+    "Sec-Ch-Ua": '"Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
+}
+
+
+def extract_idshop_iditem_from_url_tiki(url):
+    shop_id_pattern = re.compile(r"spid=(\d+)")
+    product_id_pattern = re.compile(r"p(\d+)\.html")
+
+    shop_id_match = shop_id_pattern.search(url)
+    product_id_match = product_id_pattern.search(url)
+
+    shop_id = shop_id_match.group(1) if shop_id_match else None
+    product_id = product_id_match.group(1) if product_id_match else None
+
+    return shop_id, product_id
+
+
+def fetch_tiki_ratings(product_id, spid, headers, page=1, limit=10):
+    url = "https://tiki.vn/api/v2/reviews"
+    params = {
+        "limit": limit,
+        "include": "comments,contribute_info,attribute_vote_summary",
+        "product_id": product_id,
+        "sort": "score|desc,id|desc,stars|all",
+        "page": page,
+        "spid": spid,
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        data = response.json()
+        return data.get("data", {})
+    except requests.RequestException as e:
+        print(f"Không thể lấy dữ liệu: {e}")
+        return []
+
+
+def crawl_tiki_comments(url):
+    shop_id, item_id = extract_idshop_iditem_from_url_tiki(url)
+    all_comments = []
+    if shop_id and item_id:
+        for page in range(1, 100):
+            ratings = fetch_tiki_ratings(item_id, shop_id, headers_tiki, page=page)
+            if not ratings:
+                break
+            for rating in ratings:
+                comment = rating.get("content")
+                if comment:
+                    all_comments.append(comment)
+    return all_comments
+
+
+@csrf_exempt
+def comments_tiki_analysis(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        url = data.get("url")
+        if not url:
+            return JsonResponse({"error": "url is required"}, status=400)
+
+        comments = crawl_tiki_comments(url)
+        emotion_sentiment = count_unique_emotions_sentences(comments)
+        attitude_sentiment = count_unique_attitudes_sentences(comments)
+        data_response = count_pos_neg_neu_sentences(comments)
+        pos_count = data_response["positive"]
+        neg_count = data_response["negative"]
+        neu_count = data_response["neutral"]
+
+        return JsonResponse(
+            {
+                "comments": comments,
+                "emotion_sentiment": emotion_sentiment,
+                "attitude_sentiment": attitude_sentiment,
+                "positive_count": pos_count,
+                "negative_count": neg_count,
+                "neutral_count": neu_count,
             },
             status=200,
         )
